7490b3e9120dea6e0c98a0e96fe01363df10beb4
taras <tarasdidyk94@gmail.com>
2020-09-23 10:33:43 +0300

    Add the 'Select place from the 'Search Address' dropdown' step

--- src/test/java/pages/blocks/CalculateEnergySavingsBlock.java
+++ src/test/java/pages/blocks/CalculateEnergySavingsBlock.java
@@ -1,26 +1,53 @@
 package pages.blocks;
 
-import com.codeborne.selenide.Browser;
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CalculateEnergySavingsBlock {
-    private SelenideElement calculateEnergySavingsFrame = $(By.xpath("//iframe[contains(@name, 'savings_calculator')]"));
-    private SelenideElement addressSearchField = $(By.xpath("//input[contains(@class, 'address-input')]"));
+    private final SelenideElement calculateEnergySavingsFrame = $(By.xpath("//iframe[contains(@name, 'savings_calculator') " +
+            "and contains(@class, 'xcomponent-component-frame xcomponent-visible')]"));
+    private final SelenideElement addressSearchField = $(By.xpath("//input[contains(@class, 'address-input')]"));
+    private final ElementsCollection addressSearchFieldDropdownItems = $$(By.xpath("//div[@class = 'pac-item']"));
 
+    @Step("Switch frame to the 'Calculate Energy Savings' frame")
     public CalculateEnergySavingsBlock switchToCalculateEnergySavingsFrame() {
-        switchTo().frame(calculateEnergySavingsFrame);
+        switchTo().frame(calculateEnergySavingsFrame.shouldBe(Condition.visible));
         return this;
     }
 
+    @Step("Enter an address into the 'search' field")
     public CalculateEnergySavingsBlock enterAddressIntoSearchField(String value) {
         switchToCalculateEnergySavingsFrame();
         addressSearchField.sendKeys(value);
         return this;
     }
 
+    @Step("Select place from the 'Search Address' dropdown")
+    public void selectPlaceFromTheSearchAddressDropdown(String addressName) {
+        AtomicInteger clickArrowDownTimes = new AtomicInteger(0);
+        Actions actions = actions();
+
+        addressSearchFieldDropdownItems
+                .shouldBe(CollectionCondition.sizeGreaterThan(0))
+                .stream()
+                .peek(e -> clickArrowDownTimes.incrementAndGet())
+                .filter(e -> e.getText().contains(addressName))
+                .findFirst()
+                .get();
+
+        for (int clickArrowDown = 0; clickArrowDown < clickArrowDownTimes.intValue(); clickArrowDown++) {
+            actions.sendKeys(Keys.ARROW_DOWN);
+        }
+        actions.sendKeys(Keys.ENTER).build().perform();
+    }
 }
