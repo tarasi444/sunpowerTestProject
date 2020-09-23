b3eaf41e7ace2db3435a40ddcc9f0f5f614a681b
taras <tarasdidyk94@gmail.com>
2020-09-23 15:24:21 +0300

    Clear Experimental

--- src/test/java/tests/Experimental.java
+++ src/test/java/tests/Experimental.java
@@ -1,21 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.Test;
 import tests.common.BaseTest;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
 
 public class Experimental extends BaseTest {
 
-    @Test
-    public void test() {
-        Configuration.holdBrowserOpen = true;
-        open("/");
-        mainPage.scrollAndOpenSolarBatteryStorage()
-        .scrollAndOpenCalculateEnergySavingsBlock()
-        .enterAddressIntoSearchField("test");
-        sleep(20000);
-    }
 }
