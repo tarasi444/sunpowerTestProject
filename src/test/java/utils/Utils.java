6c55f2ded34cb7cdaf71fdf5fb1316a45349f136
taras <tarasdidyk94@gmail.com>
2020-09-23 14:59:14 +0300

    Add random steps

---
+++ src/test/java/utils/Utils.java
@@ -0,0 +1,22 @@
+package utils;
+
+import io.qameta.allure.Step;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Utils {
+
+    @Step("Generate random string")
+    public static String generateRandomString(int length) {
+        return RandomStringUtils.randomAlphanumeric(length);
+    }
+
+    @Step("Generate random numeric")
+    public static String generateRandomNumeric(int length) {
+        return RandomStringUtils.randomNumeric(length);
+    }
+
+    @Step("Generate random email")
+    public static String generateRandomEmail(int length) {
+        return RandomStringUtils.randomAlphanumeric(length) + "@gmail.com";
+    }
+}
